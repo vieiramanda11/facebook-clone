6101594fcc7ed78dad403aea28dc5dff9b7ffcfa
vieiramanda11 <amanda_vieira@outlook.com>
2019-11-13 18:48:34 -0300

    creating navbar and refactoring slogin page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,4 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password)}
     end
-
-
-    def after_sign_in_path_for(resource)
-      root_path
-    end
 end
