04bf736b2fbacf2564523217e6da986b7ccf7942
vieiramanda11 <amanda_vieira@outlook.com>
2019-11-13 17:53:54 -0300

    writing methods to redirect after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,12 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password)}
-  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password)}
+    end
+
+
+    def after_sign_in_path_for(resource)
+      root_path
+    end
 end
