b6a266a3d83fdc5a2a7ec4544c72bee762e79e2b
vieiramanda11 <amanda_vieira@outlook.com>
2019-11-13 17:50:34 -0300

    refactoring configure_permitted_params method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password)}
   end
 end
