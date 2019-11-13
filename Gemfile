7c8c1a2020db70421c23ac014069ae6a407d2032
Ansar <yergeshovansar@gmail.com>
2019-11-14 02:46:36 +0600

    styled form

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'turbolinks', '~> 5'
 gem 'devise', '~> 4.7', '>= 4.7.1'
 gem 'jbuilder', '~> 2.7'
 gem 'omniauth-facebook'
+gem 'bootstrap-sass', '3.3.6'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :authenticate_user!
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password)}
