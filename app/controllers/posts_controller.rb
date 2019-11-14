93d610cd6c450456c40288d24493131b55254f33
Ansar <yergeshovansar@gmail.com>
2019-11-15 03:44:19 +0600

    current_user logic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,22 @@
 class PostsController < ApplicationController
   before_action :user_signed_in?, only: [:create,:destroy]
-
+  before_action :correct_user, only: [:edit, :destroy]
   def new
     @post = Post.new
   end
 
   def edit
     @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find_by(params[:id])
+    if @post.update(post_params)
+      flash[:success] = "Post is edited"
+      redirect_to root_path
+    else
+      render 'edit'
+    end
   end
 
   def create
@@ -33,4 +43,9 @@ class PostsController < ApplicationController
       params.require(:post).permit(:content)
     end
 
+
+    def correct_user
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to root_path if @post.nil?
+    end
 end
