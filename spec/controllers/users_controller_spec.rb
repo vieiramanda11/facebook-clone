0f9acfd9d6e976ad137c5cdbb7afdf27afce90b5
Ansar <yergeshovansar@gmail.com>
2019-11-16 03:21:22 +0600

    user controller test isn't done

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,13 +3,20 @@ require 'rails_helper'
 require_relative "../support/devise"
 
 RSpec.describe UsersController, type: :controller do 
-
-    let(:user) { FactoryBot.create(:user) }   
-
+    login_user
+    let(:user1) { FactoryBot.create(:user) }   
     describe 'GET #show' do
         it 'returns success status' do
-            get :show, params: { id: user.id }
+            get :show, params: { id: user1.id }
             expect(response).to have_http_status(:success)
+        end
+    end
+
+    describe 'Index page' do
+        
+        it 'returns success status' do
+            get 'index'
+            response.should be_success
         end
     end
   end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'support/factory_bot'
+require 'devise'
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
