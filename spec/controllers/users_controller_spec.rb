79637e5f19c87db9c5f0112e8c819c1b9c1aff38
vieiramanda11 <amanda_vieira@outlook.com>
2019-11-19 17:22:30 -0300

    writing test to succesfully logout

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,20 +3,11 @@ require 'rails_helper'
 require_relative "../support/devise"
 
 RSpec.describe UsersController, type: :controller do 
-    login_user
-    let(:user1) { FactoryBot.create(:user) }   
+    let(:user1) { FactoryBot.create(:user) }  
     describe 'GET #show' do
         it 'returns success status' do
             get :show, params: { id: user1.id }
             expect(response).to have_http_status(:success)
-        end
-    end
-
-    describe 'Index page' do
-        
-        it 'returns success status' do
-            get 'index'
-            response.should be_success
         end
     end
   end
--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -18,11 +18,16 @@ RSpec.feature "UserLogins", type: :feature do
 
   scenario ' login successfully' do
     visit new_user_session_path
-    fill_in :placeholder => "Mail", with: user1.email, :match => :prefer_exact
-    fill_in :placeholder => "Passwordd", with:  user1.password, :match => :prefer_exact
+    fill_in 'Email', with: user1.email, :match => :prefer_exact
+    fill_in 'Password', with: user1.password, :match => :prefer_exact
 
     click_button 'Log in'
     expect(page).to have_content('Signed in successfully.')
   end
 
+  scenario ' logout successfully' do
+    visit root_path
+    page.find(:xpath, "//a[@href='/users/sign_out']").click 
+    expect(page).to have_content('You need to sign in or sign up before continuing.')
+  end
 end
