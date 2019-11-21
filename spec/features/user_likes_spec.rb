f7dfcb4ba4e99f7e5686244af400a54c8b1019db
vieiramanda11 <amanda_vieira@outlook.com>
2019-11-21 19:00:45 -0300

    finishing feature like test

---
+++ spec/features/user_likes_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'UserLogouts', type: :feature do
+  let(:user1) { FactoryBot.create(:user) }
+  before do
+    user1.confirm
+  end
+  before :each do
+    visit new_user_session_path
+    fill_in 'Email', with: user1.email, match: :prefer_exact
+    fill_in 'Password', with: user1.password, match: :prefer_exact
+    click_button 'Log in'
+  end
+
+  before :each do
+    visit root_path
+    fill_in :placeholder => "What's in your mind?", :with => "Something"
+    click_button "Post"
+  end
+
+  scenario 'like post' do
+    visit root_path
+    find(:css, '.btn.btn-default').click 
+    expect(page).to have_content('1 Like')
+  end
+end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Comment, type: :model do
 
   before :each do
     @user = User.create(first_name: "name", last_name: "surename", email: "test@email.com", password: "password", password_confirmation: "password")
-    @user.reload
     @post = @user.posts.new(content: "Content text")
     @comment = Comment.new(comment: "Comment text", user: @user, post: @post)
   end 
