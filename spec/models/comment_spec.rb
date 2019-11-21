647d8a1771d390ddec3cdc6ecca8c05c0b41fef7
Ansar <yergeshovansar@gmail.com>
2019-11-22 03:36:09 +0600

    model spec done

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before :each do
+    @user = User.create(first_name: "name", last_name: "surename", email: "test@email.com", password: "password", password_confirmation: "password")
+    @user.reload
+    @post = @user.posts.new(content: "Content text")
+    @comment = Comment.new(comment: "Comment text", user: @user, post: @post)
+  end 
+  
+  it 'should be valid' do
+    expect(@comment).to be_valid
+  end
+
+  it 'comment should be present' do
+    @comment = Comment.new(comment: nil)
+    expect(@comment).to_not be_valid
+  end
+
+  it 'comment should not be too short' do
+    @comment.comment = 'a'
+    expect(@comment).to_not be_valid
+  end
+
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  @user = User.create(first_name: "name", last_name: "surename", email: "test@email.com", password: "password", password_confirmation: "password")
+  @post = @user.posts.new(content: "Content text")
+
+ 
+
+  it 'liked' do
+    @like = Like.new(user: @user, post: @post)
+    expect(@like).to be_truthy
+  end
+
+  it 'not liked' do
+    @like = Like.new(user: @user, post: @post)
+    @like.destroy
+    expect(@like).to_not be_valid
+  end
 end
+
