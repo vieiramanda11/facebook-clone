965877991d99a6e327d3a83c121262d79e55f9bc
vieiramanda11 <amanda_vieira@outlook.com>
2019-11-21 16:56:55 -0300

    finishing likes and unlike functionality

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,32 @@
 class LikesController < ApplicationController
-    def new
-        @likes = Like.new
-    end
+    before_action :find_post
+    before_action :find_like, only: [:destroy]
 
     def create
-        @post = Post.find(params[:id])
-        current_user.like(@post)
+        if liked?
+            destroy
+        else
+            @post.likes.create(user_id: current_user.id)
+        end
         redirect_to root_path
     end
 
-    def cancel
+    def destroy
+        @like.destroy
+        redirect_to root_path
+    end
+
+    private
+
+    def find_like
+        @like = @post.likes.find(params[:id])
+    end
+
+    def find_post
+        @post = Post.find(params[:post_id])
+    end
+
+    def liked? 
+        Like.where(user_id: current_user.id, post_id: params[:post_id]).exists?
     end
 end
