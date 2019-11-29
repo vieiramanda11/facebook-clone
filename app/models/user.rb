243c222d5b2b255fc75207dce5e428ce0315abae
vieiramanda11 <amanda_vieira@outlook.com>
2019-11-29 19:10:18 -0300

    fixing stickler errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,30 +16,27 @@ class User < ApplicationRecord
   has_many :inverse_friendships, -> { where(confirmed: true) }, class_name: 'Friendship', foreign_key: 'friend_id'
   has_many :inverse_friends, through: :inverse_friendships, source: :user
 
-  has_many :requests, -> { where(confirmed: false) }, class_name: 'Friendship', foreign_key: 'friend_id', source: :friend
+  has_many :requests, -> { where(confirmed: false) }, class_name: 'Friendship',
+                                                      foreign_key: 'friend_id', source: :friend
   has_many :pending_friendships, -> { where(confirmed: false) }, class_name: 'Friendship', foreign_key: 'user_id', dependent: :destroy
   mount_uploader :profile_pic, AvatarUploader
 
-  def pending_friends # for current user
+  def pending_friends
     friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
   end
 
   def friends
-    inverse_friends.joins(:friendships).where('friendships.user_id = users.id and friendships.friend_id = :self_id', self_id: id).all
+    inverse_friends.joins(:friendships)
+      .where('friendships.user_id = users.id and friendships.friend_id = :self_id', self_id: id).all
   end
 
-  def confirm_friend(user)
-    friendship = inverse_friendships.find { |friendship| friendship.user == user }
-    friendship.confirmed = true
-    friendship.save
-  end
-
-  def friend_requests # for friend
+  def friend_requests
     requests.map { |friendship| friendship.user unless friendship.confirmed }.compact
   end
 
   def friend?(friend)
-    friends = Friendship.where(user: self, friend: friend, confirmed: true) + Friendship.where(user: friend, friend: self, confirmed: true)
+    friends = Friendship.where(user: self, friend: friend, confirmed: true) +
+              Friendship.where(user: friend, friend: self, confirmed: true)
     friends.any?
   end
 
--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -29,19 +29,19 @@ RSpec.describe Friendship, type: :model do
   end
 
   it 'check if user is not confirmed friend' do
-    friendship = Friendship.new(user_id: user1.id, friend_id: user2.id)
+    Friendship.new(user_id: user1.id, friend_id: user2.id)
     value = user1.friend?(user2)
     expect(value).to be false
   end
 
   it 'check if user is requested friendship' do
-    friendship = Friendship.create(user_id: user1.id, friend_id: user2.id)
+    Friendship.create(user_id: user1.id, friend_id: user2.id)
     value = user1.pending_friends.include?(user2)
     expect(value).to be true
   end
 
   it 'check if user is requested friendship' do
-    friendship = Friendship.create(user_id: user1.id, friend_id: user2.id, confirmed: true)
+    Friendship.create(user_id: user1.id, friend_id: user2.id, confirmed: true)
     value = user2.friends.include?(user1)
     expect(value).to be true
   end
