c49be7ada2951311f4d4d1e1327ca52041cd273b
Ansar <yergeshovansar@gmail.com>
2019-12-04 23:36:48 +0600

    stickler error

--- spec/features/friendrequest2_spec.rb
+++ spec/features/friendrequest2_spec.rb
@@ -22,22 +22,17 @@ RSpec.feature 'Friendrequest2s', type: :feature do
     visit new_user_session_path
     fill_in 'Email', with: user1.email, match: :prefer_exact
     fill_in 'Password', with: user1.password, match: :prefer_exact
-
     click_button 'Log in'
   end
 
   scenario 'accept or reject friend' do
     visit users_path
-
     page.first('.btn.btn-default').click
     page.find(:xpath, "//a[@href='/users/sign_out']").click
-
     visit new_user_session_path
     fill_in 'Email', with: user2.email, match: :prefer_exact
     fill_in 'Password', with: user2.password, match: :prefer_exact
-
     click_button 'Log in'
-
     click_link('Friend Request')
     click_button 'Accept'
     expect(page).to have_content('Friend Requests 0')
--- spec/features/friendrequest_spec.rb
+++ spec/features/friendrequest_spec.rb
@@ -22,20 +22,16 @@ RSpec.feature 'Friendrequests', type: :feature do
     visit new_user_session_path
     fill_in 'Email', with: user1.email, match: :prefer_exact
     fill_in 'Password', with: user1.password, match: :prefer_exact
-
     click_button 'Log in'
   end
 
   scenario 'send friend request' do
     visit users_path
-
     page.first('.btn.btn-default').click
     page.find(:xpath, "//a[@href='/users/sign_out']").click
-
     visit new_user_session_path
     fill_in 'Email', with: user2.email, match: :prefer_exact
     fill_in 'Password', with: user2.password, match: :prefer_exact
-
     click_button 'Log in'
     expect(page).to have_content('Friend Requests 1')
   end
