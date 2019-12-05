ed0fbce08f16f25937af065c8ed1d6874b983962
vieiramanda11 <amanda_vieira@outlook.com>
2019-12-05 20:18:36 -0300

    rubocop fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,15 +45,15 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.first_name = auth.info.first_name
-      user.last_name = auth.info.last_name  
+      user.last_name = auth.info.last_name
       user.profile_pic = auth.info.profile_pic
     end
   end
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
+      if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
+        user.email = data['email'] if user.email.blank?
       end
     end
   end
