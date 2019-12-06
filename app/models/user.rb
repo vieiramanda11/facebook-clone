02b894164bb084c08b0cbba814ad15e2a93127cf
vieiramanda11 <amanda_vieira@outlook.com>
2019-12-06 16:23:43 -0300

    fixing ominiauth error in signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,11 +50,11 @@ class User < ApplicationRecord
     end
   end
 
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data == session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
-        user.email = data['email'] if user.email.blank?
-      end
-    end
-  end
+  # def self.new_with_session(params, session)
+  #   super.tap do |user|
+  #     if data == session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
+  #       user.email = data['email'] if user.email.blank?
+  #     end
+  #   end
+  # end
 end
