834ff630b02b0039bdcc57250c321038893f64ad
Ansar <yergeshovansar@gmail.com>
2019-12-07 01:46:35 +0600

    omniauth methods fixing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, controller: {
-    confirmation: 'confirmation',
+  devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
   root 'pages#index'
